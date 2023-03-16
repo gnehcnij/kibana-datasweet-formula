650972cf0d4535ba512bf0edea952347e7f98d75
gnehcnij <gnehcnij.w@qq.com>
2023-03-16 12:05:40 +0800

    去掉删除隐藏列逻辑，节省时间

--- public/decorators/lib/apply_hidden_cols.ts
+++ public/decorators/lib/apply_hidden_cols.ts
@@ -1,18 +1,10 @@
 import { includes, each, reject } from 'lodash';
-import { Datatable, DatatableRow } from '../../../../../src/plugins/expressions';
+import { Datatable } from '../../../../../src/plugins/expressions';
 // @ts-ignore
 import { TabbedAggColumn } from '../../../../../src/plugins/data/common/search/tabify/types';
 
 function mutate(table: Datatable, hiddenCols: string[]) {
   table.columns = reject(table.columns, (c) => includes(hiddenCols, c.id));
-
-  each(table.rows, (row: DatatableRow) => {
-    for (const rowKey in row) {
-      if (includes(hiddenCols, rowKey)) {
-        delete row[rowKey];
-      }
-    }
-  });
 }
 
 export function applyHiddenCols(columns: TabbedAggColumn[], resp: Datatable) {
